af89d1214b076cefe0ad941e78b3811a95d52f15
scottyb249 <scottyb249@users.noreply.github.com>
2026-04-18 15:43:38 -0400

    fix: kracronym rises image fit, updated tiles background

--- src/app/display/[gameId]/views/KracronymIntroView.tsx
+++ src/app/display/[gameId]/views/KracronymIntroView.tsx
@@ -8,12 +8,18 @@ interface Props {
 
 export default function KracronymIntroView({ game }: Props) {
   return (
-    <div className="flex flex-1 relative overflow-hidden">
-      {/* Full-bleed background image */}
+    <div className="flex flex-1 relative bg-black overflow-hidden">
       <img
         src="/kracronym-rises.png"
         alt="The Krakronym Rises"
-        className="absolute inset-0 w-full h-full object-cover"
+        style={{
+          position: 'absolute',
+          inset: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain',
+          objectPosition: 'center top',
+        }}
       />
     </div>
   )
